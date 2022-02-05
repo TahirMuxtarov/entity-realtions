b6a3336678b954101ed787e15fe473b494abd0fa
Tahir <tahir-muxtarov@mail.ru>
2022-02-05 19:03:05 +0400

    relations created

---
+++ src/main/java/com/dailycodebuffer/entity/relations/EntityRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.entity.relations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityRelationsApplication.class, args);
+	}
+
+}
